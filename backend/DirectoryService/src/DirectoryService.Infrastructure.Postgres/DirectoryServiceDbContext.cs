57db81ca9f09477570a93ad10dbc708197971e47
Michael <bigrichman7@gmail.com>
2026-08-07 21:55:39 +0300

    AI-iteration 1

--- backend/DirectoryService/src/DirectoryService.Infrastructure.Postgres/DirectoryServiceDbContext.cs
+++ backend/DirectoryService/src/DirectoryService.Infrastructure.Postgres/DirectoryServiceDbContext.cs
@@ -5,6 +5,7 @@ namespace DirectoryService.Infrastructure.Postgres;
 public class DirectoryServiceDbContext : DbContext
 {
     public DirectoryServiceDbContext(DbContextOptions<DirectoryServiceDbContext> options)
+        : base(options)
     {
          
     }
